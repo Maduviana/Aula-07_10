d14e0ff3c8df2b4fbb96f7962af5e2315ca15efe
Maduviana <112720241+Maduviana@users.noreply.github.com>
2024-10-07 21:50:53 -0300

    Add files via upload

---
+++ JS/js/objeto.js
@@ -0,0 +1,18 @@
+var objpf = {
+    nome : "Maria Eduarda",
+    idade : 18,
+    profissao : "Recepcionista",
+    trabalhando : true,
+};
+
+console.log(objpf);
+
+//Tipo de dado com typeof
+console.log(typeof objpf.trabalhando);
+
+//Acessando um objeto
+console.log(objpf.profissao)
+
+//Alterar o valor da variável
+objpf.idade = 19;
+console.log(objpf.idade);
